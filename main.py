d0b3cf640806b43bfde35beffad729887cb35ee1
hasrat97 <68140564+hasrat97@users.noreply.github.com>
2021-04-10 19:57:57 +0600

    login window

--- main.py
+++ main.py
@@ -1,8 +1,59 @@
 from tkinter import *
 import random,os
+from tkinter import messagebox,Toplevel,ttk,filedialog
+import pymysql
 
 class LoginWin:
-    pass
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Billing Sofrware - Login Window")
+        self.root.resizable(False,False)
+        self.root.geometry("1350x710+0+0")
+
+        Background= Label(self.root,bg= "purple")
+        Background.place(x=0,y=0,relwidth=1,relheight=1)
+        #=== Title ==========
+        title = Label(self.root,text="Login System",font=("arial",15,"bold"),bg="purple",fg="white",pady=10,bd=7,relief=GROOVE)
+        title.place(x=0,y=0,relwidth=1)
+
+        #========== Variables =============
+        self.manageruserVar = StringVar()
+        self.managerpassVar = StringVar()
+
+
+        # ======== Manager Login Form =============
+        managerFrame = LabelFrame(self.root,text="Login Area",font=("arial",12,"bold"),fg="gold",bd=5,bg="#115EA6",padx=50)
+        managerFrame.place(x=480,y=150,height=400)
+
+        manageruserlbl = Label(managerFrame,text="User name: ",font=("arial",13,"bold"),bg="#115EA6",fg="white").grid(row=1,column=0,padx=10,pady=20,sticky="w")
+        managerusertxt = Entry(managerFrame,textvariable=self.manageruserVar,bd=5,relief=SUNKEN,font=("arial",13,"bold")).grid(row=1,column=1,pady=20)
+
+        managerpasslbl = Label(managerFrame,text="Password: ",font=("arial",13,"bold"),bg="#115EA6",fg="white").grid(row=2,column=0,padx=10,pady=10,sticky="w")
+        managerpasstxt = Entry(managerFrame,textvariable=self.managerpassVar,bd=5,relief=SUNKEN,font=("arial",13,"bold"),show="*").grid(row=2,column=1,pady=10)
+
+        managerbtn = Button(managerFrame,command=self.Manager_log,text="Login",font=("arial",12,"bold"),width=15,pady=10,bg="black",fg="white").grid(row=3,columnspan=2,pady=15)
+
+    def Manager_log(self):
+        if self.manageruserVar.get()=="" or self.managerpassVar.get()=="":
+            messagebox.showerror("Error", "All Fields are required")
+        else:
+            try:
+                con = pymysql.connect(host="localhost", user="root", password="root1234", database="billdata")
+                cur = con.cursor()
+                q = "select * from admin where username=%s and password=%s"
+                cur.execute(q, (self.manageruserVar.get(), self.managerpassVar.get()))
+                row = cur.fetchone()
+                if row == "None":
+                    messagebox.showerror("Error", "Invalid user name or password.")
+                else:
+                    self.billingapp()
+                    self.manageruserVar.set("")
+                    self.managerpassVar.set("")
+                    self.root.withdraw()
+            except Exception as es:
+
+                messagebox.showerror("Error", f"Error due to: {str(es)}")
+
 
 root = Tk()
 obj = LoginWin(root)
